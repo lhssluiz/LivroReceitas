6c3e0f5ea466c49f1ba560e858f6ae3750fe903c
lhssluiz <lzhenriquesa@gmail.com>
2021-11-22 20:15:59 -0300

    Commit

--- src/app/app-rounting.ts
+++ src/app/app-rounting.ts
@@ -1,19 +1,23 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component"
 import { RecipesComponent } from "./recipes/recipes.component"
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component"
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component"
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/receitas', pathMatch: 'full'},
-    {   path: 'receitas', component: RecipesComponent },
-    {   path:'lista-de-comras', component: ShoppingListComponent }
+    { path: 'receitas', component: RecipesComponent,
+        children: [
+            { path: ':id', component: RecipeDetailComponent }] 
+        },
+    { path:'lista-de-comras', component: ShoppingListComponent }
 ]
 
-    @NgModule({
-        imports :  [ RouterModule.forRoot(appRoutes)],
-        exports: [RouterModule]
-    })
+@NgModule({
+    imports:  [ RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
 
 export class AppRoutingModule {
 
